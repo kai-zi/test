6e94fd863f811bed34d2268d3e68f58d2a6a7ead
unknown <2042039700@qq.com>
2016-09-13 22:43:30 +0800

    first

---
+++ app/controller/user.php
@@ -0,0 +1,10 @@
+<?php namespace app\controller;
+class User {
+
+
+	public function show() {
+
+		echo '后盾网 houdunwang.com';
+	}
+
+}
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+// require 'vendor/autoload.php';
+use app\controller\user;
+$obj = new User;
+$obj->show();
